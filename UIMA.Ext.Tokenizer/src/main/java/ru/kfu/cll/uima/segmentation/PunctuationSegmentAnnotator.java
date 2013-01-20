5a35bfd20ff60b3a0a9eca0ceccbb9f1846a1cb1
Vladimir Ivanov <nomemm@mail.ru>
2013-01-20 15:16:34 +0400

    adding UIMA Nature + RegexEnnotator

---
+++ UIMA.Ext.Tokenizer/src/main/java/ru/kfu/cll/uima/segmentation/PunctuationSegmentAnnotator.java
@@ -0,0 +1,188 @@
+package ru.kfu.cll.uima.segmentation;
+
+import static ru.kfu.itis.cll.uima.util.AnnotatorUtils.annotationTypeExist;
+
+import java.util.List;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.cas.FSIterator;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.TypeSystem;
+import org.apache.uima.cas.text.AnnotationIndex;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.util.Level;
+import org.apache.uima.util.Logger;
+import org.uimafit.component.CasAnnotator_ImplBase;
+import org.uimafit.descriptor.ConfigurationParameter;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+
+import ru.kfu.cll.uima.segmentation.fstype.PMSegment;
+import ru.kfu.cll.uima.segmentation.fstype.QSegment;
+import ru.kfu.cll.uima.segmentation.fstype.Sentence;
+import ru.kfu.cll.uima.tokenizer.fstype.PM;
+
+public class PunctuationSegmentAnnotator extends CasAnnotator_ImplBase {
+
+	@ConfigurationParameter(name = "spanAnnotationType", defaultValue = "ru.kfu.cll.uima.segmentation.fstype.Sentence")
+	private String spanAnnotationTypeName;
+	// derived
+	private Type spanAnnotationType;
+	private Logger log;
+	
+        
+	@Override
+	public void process(CAS cas) throws AnalysisEngineProcessException {
+		try {
+			process(cas.getJCas());
+		} catch (CASException e) {
+			throw new AnalysisEngineProcessException(e);
+		}
+	}
+	
+	
+
+	private void process(JCas cas) throws AnalysisEngineProcessException {
+		AnnotationIndex<Annotation> pmIndex = cas.getAnnotationIndex(PM.type);
+		AnnotationIndex<Annotation> spanIdx = cas.getAnnotationIndex(spanAnnotationType);
+		
+		FSIterator<Annotation> sentenceIterator = spanIdx.iterator();
+		sentenceIterator.moveToFirst();
+		while (sentenceIterator.isValid()) 
+		{
+			Annotation span = sentenceIterator.get();
+			process(cas, pmIndex, span);			
+			sentenceIterator.moveToNext();
+		}
+		
+		
+		
+		/*// get first sentence start
+				tokensIter.moveToFirst();
+				Annotation lastSentenceStart = tokensIter.get();
+				tokensIter.moveToNext();
+
+				while (tokensIter.isValid()) {
+					Annotation token = tokensIter.get();*/
+	}
+
+	private void process(JCas cas, AnnotationIndex<Annotation> pmIndex,
+			Annotation span) {
+	
+		FSIterator<Annotation> pmIter = pmIndex.subiterator(span);
+	
+		List<Annotation> pmList = Lists.newLinkedList();
+		info("Listing punctuation marks in the sentence:\n%s",
+				span.getCoveredText());
+		
+		while (pmIter.hasNext()) {
+			Annotation pm = pmIter.next();
+			// TODO handle other quotation marks! See TODO in the class header
+			if (pm != null && pm.getCoveredText() != null) {
+				if (pm.getCoveredText().matches("[:;,]")) 
+				{
+					pmList.add(pm);
+				}
+			}
+		}
+		
+		if (pmList.isEmpty()) 
+		{
+			// sentence = PMSegment
+			PMSegment seg1 = new PMSegment(cas);
+			seg1.setBegin(span.getBegin());
+			seg1.setEnd(span.getEnd());
+			seg1.addToIndexes();
+			return;
+		}
+
+		if (pmList.size() == 1) {
+			// 2 seg
+			info("Following span contains single punctuation mark:\n%s",
+					span.getCoveredText());
+			
+			PMSegment seg1 = new PMSegment(cas);
+			seg1.setBegin(span.getBegin());
+			seg1.setEnd(pmList.get(0).getBegin());
+			seg1.addToIndexes();
+
+			PMSegment seg2 = new PMSegment(cas);
+			seg2.setBegin(pmList.get(0).getEnd());
+			seg2.setEnd(span.getEnd());
+			seg2.addToIndexes();
+
+			return;
+		}
+
+		Annotation cur = pmList.get(0);
+		Annotation end = pmList.get(pmList.size() - 1);
+		
+		
+		info("main loop:\n%s",
+				span.getCoveredText());
+				
+		for(int i=0; i < pmList.size() - 1; i++)
+		{
+			createPMSegment(cas, cur, pmList.get(i + 1));
+			cur = pmList.get(i + 1);
+			i++;
+		}
+
+		// 2 boundary segments
+		info("postprocessing:\n%s",
+				span.getCoveredText());
+		
+		
+		PMSegment seg1 = new PMSegment(cas);
+		seg1.setBegin(span.getBegin());
+		seg1.setEnd(pmList.get(0).getBegin());
+		seg1.addToIndexes();
+
+		PMSegment seg2 = new PMSegment(cas);
+		seg2.setBegin(end.getEnd());
+		seg2.setEnd(span.getEnd());
+		seg2.addToIndexes();
+
+	}
+
+	private Annotation createPMSegment(JCas cas, Annotation pm1, Annotation pm2) {
+		// sanity check
+		/*if (pm2.getBegin() - pm1.getEnd() < 0) 
+		{			
+			throw new IllegalStateException();
+		}*/
+
+		PMSegment seg = new PMSegment(cas);
+		seg.setBegin(pm1.getEnd());
+		seg.setEnd(pm2.getBegin());
+		seg.addToIndexes();
+		return seg;
+	}
+	
+	private void info(String msg, Object... args) {
+		log.log(Level.INFO, String.format(msg, args));
+	}
+	
+	@Override
+	public void typeSystemInit(TypeSystem ts) throws AnalysisEngineProcessException {
+		super.typeSystemInit(ts);
+		spanAnnotationType = ts.getType(spanAnnotationTypeName);
+		annotationTypeExist(spanAnnotationTypeName, spanAnnotationType);
+	}
+
+	@Override
+	public void initialize(UimaContext context) throws ResourceInitializationException {
+		super.initialize(context);
+		log = context.getLogger();
+		log.setLevel(Level.INFO);
+	   
+	    info("3333333333");
+
+	}
+}
--- UIMA.Ext.Tokenizer/src/test/java/ru/kfu/cll/uima/GenerateBasicAggregateDescriptor.java
+++ UIMA.Ext.Tokenizer/src/test/java/ru/kfu/cll/uima/GenerateBasicAggregateDescriptor.java
@@ -12,13 +12,17 @@ import java.io.IOException;
 import org.apache.commons.io.IOUtils;
 import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.annotator.WhitespaceTokenizer;
+import org.apache.uima.annotator.regex.impl.RegExAnnotator;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.metadata.TypeSystemDescription;
 import org.uimafit.factory.AnalysisEngineFactory;
 import org.xml.sax.SAXException;
 
+import ru.kfu.cll.uima.segmentation.PunctuationSegmentAnnotator;
 import ru.kfu.cll.uima.segmentation.SentenceSplitter;
 import ru.kfu.cll.uima.tokenizer.InitialTokenizer;
+//import org.apache.uima.annotator.regex.impl.*;
 
 /**
  * @author Rinat Gareev (Kazan Federal University)
@@ -30,24 +34,68 @@ public class GenerateBasicAggregateDescriptor {
 	 * @param args
 	 * @throws ResourceInitializationException
 	 */
-	public static void main(String[] args) throws UIMAException, IOException, SAXException {
+	public static void main(String[] args) throws UIMAException, IOException, SAXException 
+	{
+		
+		boolean segmentator = false;
+
+		if(segmentator)
+		{
+			
 		TypeSystemDescription tokenizerTsDesc = createTypeSystemDescription("ru.kfu.cll.uima.tokenizer.tokenizer-TypeSystem");
 		AnalysisEngineDescription tokenizerDesc = createPrimitiveDescription(
 				InitialTokenizer.class, tokenizerTsDesc);
 
+//		TypeSystemDescription regTsDesc = createTypeSystemDescription("ru.kfu.cll.uima.tokenizer.tokenizer-TypeSystem-complexTypes");
+//		AnalysisEngineDescription regDesc = createPrimitiveDescription(
+//				org.apache.uima.annotator.regex.impl.RegExAnnotator.class, regTsDesc);
+
 		TypeSystemDescription ssTsDesc = createTypeSystemDescription("ru.kfu.cll.uima.segmentation.segmentation-TypeSystem");
 		AnalysisEngineDescription ssDesc = createPrimitiveDescription(SentenceSplitter.class,
 				ssTsDesc);
+		
+		TypeSystemDescription pmTsDesc = createTypeSystemDescription("ru.kfu.cll.uima.segmentation.segmentation-TypeSystem");
+		AnalysisEngineDescription pmDesc = createPrimitiveDescription(PunctuationSegmentAnnotator.class,
+				pmTsDesc);
 
-		String outputPath = "src/test/resources/basic-aggregate.xml";
+		String outputPath = "src/test/resources/token-segment-aggregate.xml";
 		AnalysisEngineDescription desc = AnalysisEngineFactory.createAggregateDescription(
-				tokenizerDesc, ssDesc);
+				tokenizerDesc, ssDesc, pmDesc);
 		FileOutputStream out = new FileOutputStream(outputPath);
 		try {
 			desc.toXML(out);
 		} finally {
 			IOUtils.closeQuietly(out);
 		}
+		
+		}
+		
+		
+		else
+		{
+			//tokenizer WS+Regex
+			
+			TypeSystemDescription tokenizerTsDesc = createTypeSystemDescription("ru.kfu.cll.uima.tokenizer.tokenizer-TypeSystem");
+			AnalysisEngineDescription tokenizerDesc = createPrimitiveDescription(
+					WhitespaceTokenizer.class, tokenizerTsDesc);
+				
+			TypeSystemDescription pmTsDesc = createTypeSystemDescription("ru.kfu.cll.uima.tokenizer.tokenizer-TypeSystem");
+			AnalysisEngineDescription pmDesc = createPrimitiveDescription(RegExAnnotator.class,
+					pmTsDesc);
+			
+			
+			String outputPath = "src/test/resources/token-ws_regex-aggregate.xml";
+			AnalysisEngineDescription desc = AnalysisEngineFactory.createAggregateDescription(
+					tokenizerDesc, pmDesc);
+			FileOutputStream out = new FileOutputStream(outputPath);
+			try {
+				desc.toXML(out);
+			} finally {
+				IOUtils.closeQuietly(out);
+			}
+			
+		}
+		
 	}
 
 }
