fb4e93a6de64f09b3de1ecac005e508aab0cb03a
Rinat Gareev <gareev-rm@yandex.ru>
2013-01-24 11:50:08 +0400

    working...

--- UIMA.Ext.FSMMatching/src/main/scala/ru/kfu/itis/issst/uima/fsmm/input/InputGraph.scala
+++ UIMA.Ext.FSMMatching/src/main/scala/ru/kfu/itis/issst/uima/fsmm/input/InputGraph.scala
@@ -9,10 +9,14 @@ package ru.kfu.itis.issst.uima.fsmm.input
  *
  */
 trait InputGraph[A] {
- 
+
   /**
-   * return the set of annotations that have begin at the first (non-empty) offset
+   * return the tuple:
+   * 	1. the set of annotations starting from F
+   * 	where F is minimal offset strictly > from and have annotations
+   * 	2. F
+   * If there are no such annotation return (empty set, from)
    */
-  def getFirst():Set[A]
+  def next(from: Int): (Set[A], Int)
 
 }
--- UIMA.Ext.FSMMatching/src/main/scala/ru/kfu/itis/issst/uima/fsmm/nfsm/NFiniteStateMachine.scala
+++ UIMA.Ext.FSMMatching/src/main/scala/ru/kfu/itis/issst/uima/fsmm/nfsm/NFiniteStateMachine.scala
@@ -7,7 +7,7 @@ import ru.kfu.itis.issst.uima.fsmm.input.InputGraph
 
 /**
  * Represents a non-deterministic finite state machine
- * @tparam A input annotation type
+ * @tparam A input element type
  * @author Rinat Gareev (Kazan Federal University)
  *
  */
@@ -15,8 +15,32 @@ class NFiniteStateMachine[A](val initialState: State) {
   require(initialState.isBuilt, "Can't construct NDFSM from non-finished initial state")
 
   def process(input: InputGraph[A]) {
-    // TODO implement
+    process(input, -1)
   }
+
+  private def process(input: InputGraph[A], from: Int) {
+    val (annoSet, annoSetBegin) = input.next(from)
+    annoSet.foreach(process _)
+    if (!annoSet.isEmpty)
+      process(input, annoSetBegin)
+  }
+  
+  private def process(anno:A) {
+    val track = new Track()
+  }
+
+  /**
+   * Immutable state of matching
+   */
+  private class Track(val consumedInput: List[TrackElement]) {
+    def this() = this(Nil)
+
+    def prepend(head: TrackElement): Track = new Track(head :: consumedInput)
+  }
+
+  private sealed abstract class TrackElement
+  private case class ConsumedInputElement[A](input: A) extends TrackElement
+  private case class LabelBorder(label: String, begin: Boolean) extends TrackElement
 }
 
 /*
@@ -41,4 +65,4 @@ private[nfsm] class State {
   private[nfsm] def isBuilt = built
 }
 
-private[nfsm] class Transition(from: State, to: State, input: InputMatcher)
+private[nfsm] class Transition(from: State, to: State, input: InputMatcher) 
