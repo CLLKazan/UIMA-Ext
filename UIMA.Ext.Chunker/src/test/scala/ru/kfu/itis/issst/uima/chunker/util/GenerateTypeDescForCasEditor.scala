aeb23bb25385304a87d12a095ded2f8812cfb92e
Rinat Gareev <gareev-rm@yandex.ru>
2013-02-04 18:13:37 +0400

    preparing test resources for VP-chunker

---
+++ UIMA.Ext.Chunker/src/test/scala/ru/kfu/itis/issst/uima/chunker/util/GenerateTypeDescForCasEditor.scala
@@ -0,0 +1,31 @@
+/**
+ *
+ */
+package ru.kfu.itis.issst.uima.chunker.util
+
+import org.uimafit.factory.TypeSystemDescriptionFactory
+import org.apache.uima.util.CasCreationUtils
+import java.util.Arrays
+import java.io.FileOutputStream
+import org.apache.commons.io.IOUtils
+
+/**
+ * @author Rinat Gareev
+ *
+ */
+object GenerateTypeDescForCasEditor {
+
+  def main(args: Array[String]) {
+    val outPath = "desc/types/TypeSystem4CasViewer.xml";
+    var tsDesc = TypeSystemDescriptionFactory.createTypeSystemDescription(
+      "ru.kfu.itis.issst.uima.chunker.ts-chunking",
+      "ru.kfu.itis.cll.uima.commons.Commons-TypeSystem");
+    tsDesc = CasCreationUtils.mergeTypeSystems(Arrays.asList(tsDesc));
+    val fos = new FileOutputStream(outPath);
+    try {
+      tsDesc.toXML(fos);
+    } finally {
+      IOUtils.closeQuietly(fos);
+    }
+  }
+}
