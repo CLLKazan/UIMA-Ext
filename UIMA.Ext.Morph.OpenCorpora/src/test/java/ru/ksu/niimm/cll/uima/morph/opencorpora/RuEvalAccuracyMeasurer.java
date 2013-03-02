83822f651cc64ee6f8f20ce7fd3ed43e74585eab
Fedor Nikolaev <fsqcds@gmail.com>
2013-03-02 22:43:24 +0400

    RuEval grammems measuring

--- UIMA.Ext.Morph.OpenCorpora/src/test/java/ru/ksu/niimm/cll/uima/morph/opencorpora/RuEvalAccuracyMeasurer.java
+++ UIMA.Ext.Morph.OpenCorpora/src/test/java/ru/ksu/niimm/cll/uima/morph/opencorpora/RuEvalAccuracyMeasurer.java
@@ -29,7 +29,7 @@ import java.util.Set;
  * To change this template use File | Settings | File Templates.
  */
 public class RuEvalAccuracyMeasurer {
-    private static String documentText;
+    private static String documentText = "";
     private static Map<String, String> ruEvalPOS = Maps.newHashMap();
     private static Map<String, Set<String>> ruEvalGram = Maps.newHashMap();
 
@@ -92,16 +92,22 @@ public class RuEvalAccuracyMeasurer {
             org.opencorpora.cas.Wordform casWf = word.getWordforms(i);
             String pos = casWf.getPos();
             String[] grammemsArray = new String[casWf.getGrammems().size()];
-            casWf.getGrammems().copyFromArray(grammemsArray, 0, 0, grammemsArray.length);
+            casWf.getGrammems().copyToArray(0, grammemsArray, 0, grammemsArray.length);
             Set<String> grammems = new HashSet<String>(Arrays.asList(grammemsArray));
 
+            if (i > 0) System.out.println("------");
             System.out.println(wordStr);
             System.out.println(ruEvalPOS.get(wordStr));
             System.out.println(pos);
-            System.out.println(opCorporaPOStoRuEval(pos, grammems));
+            System.out.println(opCorporaPOSToRuEval(pos, grammems));
             System.out.println();
-            if (opCorporaPOStoRuEval(pos, grammems).equals(ruEvalPOS.get(wordStr)))
+            System.out.println(ruEvalGram.get(wordStr));
+            System.out.println(grammems);
+            System.out.println(opCorporaGramToRuEval(pos, grammems));
+            if (opCorporaPOSToRuEval(pos, grammems).equals(ruEvalPOS.get(wordStr)) &&
+                opCorporaGramToRuEval(pos, grammems).containsAll(ruEvalGram.get(wordStr))) {
                 return true;
+            }
         }
         return false;
     }
@@ -119,7 +125,7 @@ public class RuEvalAccuracyMeasurer {
         ruEvalGram.put(word, new HashSet<String>(Arrays.asList(gram)));
     }
 
-    private static String opCorporaPOStoRuEval(String ocPOS, Set<String> ocGrammems) {
+    private static String opCorporaPOSToRuEval(String ocPOS, Set<String> ocGrammems) {
         if (ocPOS.equals("NOUN")) {
             return "S";
         } else if (ocPOS.equals("ADJF") || ocPOS.equals("ADJS")) {
@@ -136,5 +142,70 @@ public class RuEvalAccuracyMeasurer {
             return "ADV";
         }
         return ocPOS;
+    }
+
+    private static Set<String> opCorporaGramToRuEval(String ocPOS, Set<String> ocGrammems) {
+        Set<String> result = new HashSet<String>();
+        if (ocGrammems.contains("masc")) {
+            result.add("m");
+        } else if (ocGrammems.contains("femn")) {
+            result.add("f");
+        } else if (ocGrammems.contains("neut")) {
+            result.add("n");
+        }
+
+        if (ocGrammems.contains("nomn") || ocGrammems.contains("voct")) {
+            result.add("nom");
+        } else if (ocGrammems.contains("gent") || ocGrammems.contains("gen1") || ocGrammems.contains("gen2")) {
+            result.add("gen");
+        } else if (ocGrammems.contains("datv")) {
+            result.add("dat");
+        } else if (ocGrammems.contains("accs") || ocGrammems.contains("acc2")) {
+            result.add("acc");
+        } else if (ocGrammems.contains("ablt")) {
+            result.add("ins");
+        } else if (ocGrammems.contains("loct") || ocGrammems.contains("loc1") || ocGrammems.contains("loc2")) {
+            result.add("loc");
+        }
+
+        if (ocGrammems.contains("sing")) {
+            result.add("sg");
+        } else if (ocGrammems.contains("plur")) {
+            result.add("pl");
+        }
+
+        if (ocGrammems.contains("pres") || ocGrammems.contains("futr")) {
+            result.add("pres");
+        } else if (ocGrammems.contains("past")) {
+            result.add("past");
+        }
+
+        if (ocGrammems.contains("impr")) {
+            result.add("imper");
+        }
+
+        if (ocPOS.equals("INFN")) {
+            result.add("inf");
+        } else if (ocPOS.equals("PRTF") || ocPOS.equals("PRTS")) {
+            result.add("partcp");
+        } else if (ocPOS.equals("GRND")) {
+            result.add("ger");
+        }
+
+        if (ocGrammems.contains("actv")) {
+            result.add("act");
+        } else if (ocGrammems.contains("pssv")) {
+            result.add("pass");
+        }
+
+        if (ocGrammems.contains("1per")) {
+            result.add("1p");
+        } else if (ocGrammems.contains("2per")) {
+            result.add("2p");
+        } else if (ocGrammems.contains("3per")) {
+            result.add("3p");
+        }
+
+        return result;
     }
 }
