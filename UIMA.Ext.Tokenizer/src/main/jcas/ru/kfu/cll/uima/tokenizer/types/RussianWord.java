ff1219d6180f88cf395960a37785aade5a92a2e2
MarselkaTheBest <marsel@marsel-P5K.(none)>
2013-03-26 17:55:13 +0400

    Added RussianWord-type

---
+++ UIMA.Ext.Tokenizer/src/main/jcas/ru/kfu/cll/uima/tokenizer/types/RussianWord.java
@@ -0,0 +1,64 @@
+
+
+/* First created by JCasGen Tue Mar 26 17:45:19 SAMT 2013 */
+package ru.kfu.cll.uima.tokenizer.types;
+
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+
+
+
+/** 
+ * Updated by JCasGen Tue Mar 26 17:45:19 SAMT 2013
+ * XML source: /home/fsqcds/idea-projects/UIMA-Ext/UIMA.Ext.Tokenizer/src/main/resources/ru/kfu/cll/uima/tokenizer/jflex-tokenizer-ts.xml
+ * @generated */
+public class RussianWord extends Token {
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = JCasRegistry.register(RussianWord.class);
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int type = typeIndexID;
+  /** @generated  */
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected RussianWord() {/* intentionally empty block */}
+    
+  /** Internal - constructor used by generator 
+   * @generated */
+  public RussianWord(int addr, TOP_Type type) {
+    super(addr, type);
+    readObject();
+  }
+  
+  /** @generated */
+  public RussianWord(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+  /** @generated */  
+  public RussianWord(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** <!-- begin-user-doc -->
+    * Write your own initialization here
+    * <!-- end-user-doc -->
+  @generated modifiable */
+  private void readObject() {/*default - does nothing empty block */}
+     
+}
+
+    
