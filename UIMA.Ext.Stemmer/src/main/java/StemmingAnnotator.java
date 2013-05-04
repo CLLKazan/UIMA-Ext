8bf51adb19eb9862f2b2e851e4a2ff219a645a0d
MarselSidikov <sidikov.marsel@gmail.com>
2013-05-04 22:56:12 +0400

    added main class of stemmer

---
+++ UIMA.Ext.Stemmer/src/main/java/StemmingAnnotator.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: marsel
+ * Date: 04.05.13
+ * Time: 22:48
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+public class StemmingAnnotator {
+}
