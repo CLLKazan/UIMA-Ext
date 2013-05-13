ed8d2d3a05b6528890ce917053f43012e996ed82
Rinat Gareev <gareev-rm@yandex.ru>
2013-05-14 02:31:47 +0400

    [UIMA.Ext.Commons] + the method that checks preconditions in AnnotationOffsetComparator

--- UIMA.Ext.Commons/src/main/java/ru/kfu/itis/cll/uima/cas/AnnotationOffsetComparator.java
+++ UIMA.Ext.Commons/src/main/java/ru/kfu/itis/cll/uima/cas/AnnotationOffsetComparator.java
@@ -5,7 +5,11 @@ package ru.kfu.itis.cll.uima.cas;
 
 import java.util.Comparator;
 
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.FSIterator;
+import org.apache.uima.cas.Type;
 import org.apache.uima.cas.text.AnnotationFS;
+import org.uimafit.util.CasUtil;
 
 /**
  * @author Rinat Gareev
@@ -15,6 +19,28 @@ public class AnnotationOffsetComparator<A extends AnnotationFS> implements Compa
 
 	public static <A extends AnnotationFS> AnnotationOffsetComparator<A> instance(Class<A> clazz) {
 		return new AnnotationOffsetComparator<A>();
+	}
+
+	public static void checkForTheSameBoundaries(CAS cas, Class<? extends AnnotationFS> typeClass) {
+		Type type = CasUtil.getType(cas, typeClass);
+		FSIterator<AnnotationFS> iter = cas.getAnnotationIndex(type).iterator();
+		iter.moveToFirst();
+		if (!iter.isValid()) {
+			return;
+		}
+		AnnotationOffsetComparator<AnnotationFS> cmp =
+				AnnotationOffsetComparator.instance(AnnotationFS.class);
+		AnnotationFS lastAnno = iter.get();
+		iter.moveToNext();
+		while (iter.isValid()) {
+			AnnotationFS anno = iter.get();
+			if (cmp.compare(anno, lastAnno) == 0) {
+				throw new IllegalStateException(String.format(
+						"Annotations %s and %s have the same boundaries",
+						lastAnno, anno));
+			}
+			iter.moveToNext();
+		}
 	}
 
 	@Override
