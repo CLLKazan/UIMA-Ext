6a2001b7f0d5f117ca615b9ad98ff731ce707f12
MarselSidikov <sidikov.marsel@gmail.com>
2013-05-22 01:29:56 +0400

    added types in TokenGenerator

--- UIMA.Ext.Tokenizer/src/main/java/ru/kfu/cll/uima/tokenizer/TokenGenerator.java
+++ UIMA.Ext.Tokenizer/src/main/java/ru/kfu/cll/uima/tokenizer/TokenGenerator.java
@@ -1,5 +1,23 @@
 package ru.kfu.cll.uima.tokenizer;
 
+import ru.kfu.cll.uima.tokenizer.types.Token;
+import ru.kfu.cll.uima.tokenizer.types.Letters;
+import ru.kfu.cll.uima.tokenizer.types.Number;
+import ru.kfu.cll.uima.tokenizer.types.Separator;
+import ru.kfu.cll.uima.tokenizer.types.PM;
+import ru.kfu.cll.uima.tokenizer.types.Bracket;
+import ru.kfu.cll.uima.tokenizer.types.Symbol;
+import ru.kfu.cll.uima.tokenizer.types.Range;
+import ru.kfu.cll.uima.tokenizer.types.Abbrevation;
+import ru.kfu.cll.uima.tokenizer.types.Currensy;
+import ru.kfu.cll.uima.tokenizer.types.Measurement;
+import ru.kfu.cll.uima.tokenizer.types.ComplexWord;
+import ru.kfu.cll.uima.tokenizer.types.Date;
+import ru.kfu.cll.uima.tokenizer.types.Email;
+import ru.kfu.cll.uima.tokenizer.types.Abbrevation;
+import ru.kfu.cll.uima.tokenizer.types.RussianWord;
+import org.apache.uima.jcas.JCas;
+
 /**
  * Created with IntelliJ IDEA.
  * User: marsel
