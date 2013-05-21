b1d3d51084b5908d98b8abd2c240cb2ae66cb84e
MarselSidikov <sidikov.marsel@gmail.com>
2013-05-22 01:23:05 +0400

    added TokenGenerator

---
+++ UIMA.Ext.Tokenizer/src/main/java/ru/kfu/cll/uima/tokenizer/TokenGenerator.java
@@ -0,0 +1,11 @@
+package ru.kfu.cll.uima.tokenizer;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: marsel
+ * Date: 22.05.13
+ * Time: 1:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class TokenGenerator {
+}
