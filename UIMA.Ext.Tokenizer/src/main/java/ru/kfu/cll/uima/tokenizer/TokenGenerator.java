1a1278dd2edbf37140be092298a3d99eb147faaa
MarselSidikov <sidikov.marsel@gmail.com>
2013-05-22 20:40:17 +0400

    added JCas-field, constructor and Generate-method withtwo types - Letters and Number

--- UIMA.Ext.Tokenizer/src/main/java/ru/kfu/cll/uima/tokenizer/TokenGenerator.java
+++ UIMA.Ext.Tokenizer/src/main/java/ru/kfu/cll/uima/tokenizer/TokenGenerator.java
@@ -26,4 +26,30 @@ import org.apache.uima.jcas.JCas;
  * To change this template use File | Settings | File Templates.
  */
 public class TokenGenerator {
+    private JCas UIMA_JCas;
+    public TokenGenerator(JCas UIMA_JCas) {
+        this.UIMA_JCas = UIMA_JCas;
+    }
+
+    public Token Generate(String typeOfToken, int begin, int end, String args[]) {
+        Token token = new Token(UIMA_JCas);
+        token.setNorm(null);
+        token.setText(args[0]);
+        token.setBegin(begin);
+        token.setEnd(end);
+        if (typeOfToken.equals("Letters")) {
+            Letters letters = (Letters) token;
+            letters.setLanguage(args[1]);
+            letters.setLetterCase(args[2]);
+            return letters;
+        } else if (typeOfToken.equals("Number")) {
+            Number number = (Number) token;
+            number.setKind(args[1]);
+            number.setSign(args[2]);
+            return number;
+        }
+        else {
+            return null;
+        }
+    }
 }
