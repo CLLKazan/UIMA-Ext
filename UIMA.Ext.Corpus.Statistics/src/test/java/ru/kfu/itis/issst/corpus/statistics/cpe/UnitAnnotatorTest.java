e8ced2bfebe5f6f97ac504e0789387c401d2d21b
fsqcds <fsqcds@gmail.com>
2014-04-06 21:51:42 +0400

    Improve UnitAnnotatorTest

--- UIMA.Ext.Corpus.Statistics/src/test/java/ru/kfu/itis/issst/corpus/statistics/cpe/UnitAnnotatorTest.java
+++ UIMA.Ext.Corpus.Statistics/src/test/java/ru/kfu/itis/issst/corpus/statistics/cpe/UnitAnnotatorTest.java
@@ -1,19 +1,27 @@
 package ru.kfu.itis.issst.corpus.statistics.cpe;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 import java.util.Set;
 
 import org.apache.uima.UIMAException;
-import org.apache.uima.analysis_engine.AnalysisEngineDescription;
-import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.Type;
+import org.apache.uima.collection.CollectionReader;
 import org.apache.uima.collection.metadata.CpeDescriptorException;
+import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ExternalResourceDescription;
 import org.junit.Before;
 import org.junit.Test;
 import org.uimafit.factory.AnalysisEngineFactory;
 import org.uimafit.factory.CollectionReaderFactory;
 import org.uimafit.factory.ExternalResourceFactory;
-import org.uimafit.pipeline.SimplePipeline;
+import org.uimafit.pipeline.JCasIterable;
+import org.uimafit.util.CasUtil;
 import org.xml.sax.SAXException;
 
 import ru.kfu.itis.issst.corpus.statistics.dao.XmiFileTreeCorpusDAO;
@@ -21,23 +29,26 @@ import ru.kfu.itis.issst.corpus.statistics.dao.XmiFileTreeCorpusDAO;
 import com.google.common.collect.Sets;
 
 public class UnitAnnotatorTest {
-	String corpusPathString = Thread.currentThread().getContextClassLoader().getResource("corpus_example").getPath();
-	Set<String> unitTypes = Sets.newHashSet("ru.kfu.cll.uima.tokenizer.fstype.W");
+	String corpusPathString = Thread.currentThread().getContextClassLoader()
+			.getResource("corpus_example").getPath();
+	Set<String> unitTypes = Sets
+			.newHashSet("ru.kfu.cll.uima.tokenizer.fstype.W");
 	ExternalResourceDescription daoDesc;
-	CollectionReaderDescription reader;
-	AnalysisEngineDescription tokenizerSentenceSplitter;
-	AnalysisEngineDescription unitAnnotator;
+	CollectionReader reader;
+	AnalysisEngine tokenizerSentenceSplitter;
+	AnalysisEngine unitAnnotator;
 
 	@Before
 	public void setUp() throws Exception {
 		daoDesc = ExternalResourceFactory.createExternalResourceDescription(
 				XmiFileTreeCorpusDAOResource.class, corpusPathString);
-		reader = CollectionReaderFactory.createDescription(
+		reader = CollectionReaderFactory.createCollectionReader(
 				CorpusDAOCollectionReader.class,
 				XmiFileTreeCorpusDAO.getTypeSystem(corpusPathString),
 				CorpusDAOCollectionReader.CORPUS_DAO_KEY, daoDesc);
-		tokenizerSentenceSplitter = Unitizer.createTokenizerSentenceSplitterAED();
-		unitAnnotator = AnalysisEngineFactory.createPrimitiveDescription(
+		tokenizerSentenceSplitter = AnalysisEngineFactory
+				.createAggregate(Unitizer.createTokenizerSentenceSplitterAED());
+		unitAnnotator = AnalysisEngineFactory.createPrimitive(
 				UnitAnnotator.class, UnitAnnotator.PARAM_UNIT_TYPE_NAMES,
 				unitTypes);
 	}
@@ -45,7 +56,18 @@ public class UnitAnnotatorTest {
 	@Test
 	public void test() throws UIMAException, SAXException,
 			CpeDescriptorException, IOException {
-		SimplePipeline.runPipeline(reader, tokenizerSentenceSplitter, unitAnnotator);
-	}
+		for (JCas jCas : new JCasIterable(reader, tokenizerSentenceSplitter,
+				unitAnnotator)) {
+			CAS aCas = jCas.getCas();
+			Type unitSourceType = CasUtil.getType(aCas, unitTypes.iterator()
+					.next());
+			Type unitType = aCas.getTypeSystem().getType(
+					UnitAnnotator.UNIT_TYPE_NAME);
 
+			int sourceNumber = CasUtil.select(aCas, unitSourceType).size();
+			int unitNumber = CasUtil.select(aCas, unitType).size();
+			assertTrue(sourceNumber > 0);
+			assertThat(unitNumber, equalTo(sourceNumber));
+		}
+	}
 }
