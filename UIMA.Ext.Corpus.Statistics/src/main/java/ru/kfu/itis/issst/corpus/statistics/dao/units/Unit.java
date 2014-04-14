b0691017e799edbadada53dfc983e60ad630b7f3
fsqcds <fsqcds@gmail.com>
2014-04-14 20:50:55 +0400

    UnitLocation

--- UIMA.Ext.Corpus.Statistics/src/main/java/ru/kfu/itis/issst/corpus/statistics/dao/units/Unit.java
+++ UIMA.Ext.Corpus.Statistics/src/main/java/ru/kfu/itis/issst/corpus/statistics/dao/units/Unit.java
@@ -1,54 +1,43 @@
 package ru.kfu.itis.issst.corpus.statistics.dao.units;
 
 import java.net.URI;
-import java.util.Map;
 import java.util.SortedMap;
 
 import com.google.common.collect.Maps;
 
 public class Unit {
 
-	private URI documentURI;
-	private int begin;
-	private int end;
+	private UnitLocation location;
 	private SortedMap<String, String> classByAnnotatorId = Maps.newTreeMap();
 
 	public Unit(URI documentURI, int begin, int end) {
-		this.documentURI = documentURI;
-		this.begin = begin;
-		this.end = end;
+		location = new UnitLocation(documentURI, begin, end);
 	}
 
 	public Unit(URI documentURI, int begin, int end, String annotatorId,
 			String annotatorClass) {
-		this.documentURI = documentURI;
-		this.begin = begin;
-		this.end = end;
+		this(documentURI, begin, end);
 		classByAnnotatorId.put(annotatorId, annotatorClass);
 	}
 
+	public UnitLocation getLocation() {
+		return location;
+	}
+
 	public URI getDocumentURI() {
-		return documentURI;
+		return location.getDocumentURI();
 	}
 
 	public int getBegin() {
-		return begin;
+		return location.getBegin();
 	}
 
 	public int getEnd() {
-		return end;
-	}
-
-	public Map<String, String> getClassesByAnnotatorId() {
-		return classByAnnotatorId;
+		return location.getEnd();
 	}
 
 	public String[] getSortedClasses() {
 		return classByAnnotatorId.values().toArray(new String[0]);
-	}
-
-	public String getClassByAnnotatorId(String annotatorId) {
-		return classByAnnotatorId.get(annotatorId);
 	}
 
 	public void putClassByAnnotatorId(String annotatorId, String annotatorClass) {
---
+++ UIMA.Ext.Corpus.Statistics/src/main/java/ru/kfu/itis/issst/corpus/statistics/dao/units/UnitLocation.java
@@ -0,0 +1,61 @@
+package ru.kfu.itis.issst.corpus.statistics.dao.units;
+
+import java.net.URI;
+
+public class UnitLocation {
+
+	public URI documentURI;
+	public int begin;
+	public int end;
+
+	public URI getDocumentURI() {
+		return documentURI;
+	}
+
+	public int getBegin() {
+		return begin;
+	}
+
+	public int getEnd() {
+		return end;
+	}
+
+	public UnitLocation(URI documentURI, int begin, int end) {
+		this.documentURI = documentURI;
+		this.begin = begin;
+		this.end = end;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + begin;
+		result = prime * result
+				+ ((documentURI == null) ? 0 : documentURI.hashCode());
+		result = prime * result + end;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UnitLocation other = (UnitLocation) obj;
+		if (begin != other.begin)
+			return false;
+		if (documentURI == null) {
+			if (other.documentURI != null)
+				return false;
+		} else if (!documentURI.equals(other.documentURI))
+			return false;
+		if (end != other.end)
+			return false;
+		return true;
+	}
+
+}
