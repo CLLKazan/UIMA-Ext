7aef88239f5617dd8889ae702fc2d2e05002ae10
fsqcds <fsqcds@gmail.com>
2014-05-16 20:29:57 +0400

    Inject OpenCorpora dictionary to lemmatizer

--- UIMA.Ext.Morph.Lemmatizer/src/main/scala/ru/kfu/itis/issst/uima/morph/lemmatizer/Lemmatizer.scala
+++ UIMA.Ext.Morph.Lemmatizer/src/main/scala/ru/kfu/itis/issst/uima/morph/lemmatizer/Lemmatizer.scala
@@ -3,15 +3,27 @@ package ru.kfu.itis.issst.uima.morph.lemmatizer
 import scala.collection.JavaConversions._
 import org.apache.uima.jcas.JCas
 import org.uimafit.util.JCasUtil.select
-import org.opencorpora.cas.Wordform
+import org.opencorpora.cas.{Word,Wordform}
+import org.apache.uima.cas.FeatureStructure
+import org.uimafit.descriptor.ExternalResource
+import ru.ksu.niimm.cll.uima.morph.opencorpora.resource.MorphDictionaryHolder
+import ru.ksu.niimm.cll.uima.morph.opencorpora.MorphologyAnnotator
 
 /**
  * Created by fsqcds on 07/05/14.
  */
-class Lemmatizer extends org.uimafit.component.JCasAnnotator_ImplBase {
+class Lemmatizer() extends org.uimafit.component.JCasAnnotator_ImplBase {
+  @ExternalResource(key = MorphologyAnnotator.RESOURCE_KEY_DICTIONARY)
+  private var dictHolder: MorphDictionaryHolder = null
+
   def process(aJCAS : JCas) {
-    select(aJCAS, classOf[Wordform]).foreach((wf: Wordform) =>
-      println(wf.getGrammems)
-    )
+    select(aJCAS, classOf[Word]).foreach((word: Word) => {
+      val wordText = word.getCoveredText
+      println(dictHolder.getDictionary.getEntries(wordText))
+      println(wordText)
+      word.getWordforms.toArray.foreach((wordform: FeatureStructure) =>
+        println(wordform.asInstanceOf[Wordform].getGrammems)
+      )
+    })
   }
 }
--- UIMA.Ext.Morph.Lemmatizer/src/test/scala/ru/kfu/itis/issst/uima/morph/lemmatizer/LemmatizerSpec.scala
+++ UIMA.Ext.Morph.Lemmatizer/src/test/scala/ru/kfu/itis/issst/uima/morph/lemmatizer/LemmatizerSpec.scala
@@ -5,17 +5,23 @@ import ru.ksu.niimm.cll.uima.morph.ml.GeneratePipelineDescriptorForTPSAnnotator
 import org.uimafit.factory.AnalysisEngineFactory
 import org.uimafit.pipeline.SimplePipeline
 import org.apache.uima.util.CasCreationUtils
+import org.uimafit.factory.ExternalResourceFactory.{createExternalResourceDescription, createDependencyAndBind}
+import ru.ksu.niimm.cll.uima.morph.opencorpora.resource.{CachedSerializedDictionaryResource, MorphDictionaryHolder}
+import java.io.File
+import ru.ksu.niimm.cll.uima.morph.opencorpora.MorphologyAnnotator
+import java.util.Arrays
 
 /**
  * Created by fsqcds on 07/05/14.
  */
 class LemmatizerSpec extends FlatSpec with Matchers {
-  "Lemmatizer" should "do something" in {
-    val posPipelineDesc = GeneratePipelineDescriptorForTPSAnnotator.getDescription;
-    val lemmatizerDesc = AnalysisEngineFactory.createPrimitiveDescription(classOf[Lemmatizer])
+  "Lemmatizer" should "be runnable" in {
+    val posPipelineDesc = GeneratePipelineDescriptorForTPSAnnotator.getDescription
+    val extDictDesc = createExternalResourceDescription(classOf[CachedSerializedDictionaryResource], new File("dict.opcorpora.ser"))
+    val lemmatizerDesc = AnalysisEngineFactory.createPrimitiveDescription(classOf[Lemmatizer], MorphologyAnnotator.RESOURCE_KEY_DICTIONARY, extDictDesc)
     val aggregateDesc = AnalysisEngineFactory.createAggregateDescription(posPipelineDesc, lemmatizerDesc)
-    val jCas = CasCreationUtils.createCas(aggregateDesc).getJCas;
+    val jCas = CasCreationUtils.createCas(aggregateDesc).getJCas
     jCas.setDocumentText("Какой-то русский текст написан здесь.")
-    SimplePipeline.runPipeline(jCas, posPipelineDesc, lemmatizerDesc)
+    SimplePipeline.runPipeline(jCas, aggregateDesc)
   }
 }
