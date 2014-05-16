536780c874bf975edc039221c1c4c73c0ea44be5
fsqcds <fsqcds@gmail.com>
2014-05-16 21:17:14 +0400

    Naive lemmatizer implementation and failed spec

--- UIMA.Ext.Morph.Lemmatizer/src/main/scala/ru/kfu/itis/issst/uima/morph/lemmatizer/Lemmatizer.scala
+++ UIMA.Ext.Morph.Lemmatizer/src/main/scala/ru/kfu/itis/issst/uima/morph/lemmatizer/Lemmatizer.scala
@@ -3,11 +3,11 @@ package ru.kfu.itis.issst.uima.morph.lemmatizer
 import scala.collection.JavaConversions._
 import org.apache.uima.jcas.JCas
 import org.uimafit.util.JCasUtil.select
-import org.opencorpora.cas.{Word,Wordform}
+import org.opencorpora.cas.{Word, Wordform}
 import org.apache.uima.cas.FeatureStructure
 import org.uimafit.descriptor.ExternalResource
 import ru.ksu.niimm.cll.uima.morph.opencorpora.resource.MorphDictionaryHolder
-import ru.ksu.niimm.cll.uima.morph.opencorpora.MorphologyAnnotator
+import ru.ksu.niimm.cll.uima.morph.opencorpora.{WordUtils, MorphologyAnnotator}
 
 /**
  * Created by fsqcds on 07/05/14.
@@ -16,14 +16,24 @@ class Lemmatizer() extends org.uimafit.component.JCasAnnotator_ImplBase {
   @ExternalResource(key = MorphologyAnnotator.RESOURCE_KEY_DICTIONARY)
   private var dictHolder: MorphDictionaryHolder = null
 
-  def process(aJCAS : JCas) {
+  def findLemma(wordform: Wordform): String = {
+    val lemmaId = dictHolder.getDictionary.getEntries(WordUtils.normalizeToDictionaryForm
+      (wordform.getWord.getCoveredText)).get(0).getLemmaId
+    dictHolder.getDictionary.getLemma(lemmaId).getString
+  }
+
+  def process(aJCAS: JCas) {
     select(aJCAS, classOf[Word]).foreach((word: Word) => {
-      val wordText = word.getCoveredText
-      println(dictHolder.getDictionary.getEntries(wordText))
-      println(wordText)
-      word.getWordforms.toArray.foreach((wordform: FeatureStructure) =>
-        println(wordform.asInstanceOf[Wordform].getGrammems)
-      )
+      word.getWordforms.toArray.foreach((wordformFS: FeatureStructure) => {
+        val wordform = wordformFS.asInstanceOf[Wordform]
+        try {
+          val lemma = findLemma(wordform)
+          wordform.setLemma(lemma)
+        }
+        catch {
+          case e: IndexOutOfBoundsException => {}
+        }
+      })
     })
   }
 }
--- UIMA.Ext.Morph.Lemmatizer/src/test/scala/ru/kfu/itis/issst/uima/morph/lemmatizer/LemmatizerSpec.scala
+++ UIMA.Ext.Morph.Lemmatizer/src/test/scala/ru/kfu/itis/issst/uima/morph/lemmatizer/LemmatizerSpec.scala
@@ -1,27 +1,36 @@
 package ru.kfu.itis.issst.uima.morph.lemmatizer
 
+import scala.collection.JavaConversions._
 import org.scalatest._
 import ru.ksu.niimm.cll.uima.morph.ml.GeneratePipelineDescriptorForTPSAnnotator
 import org.uimafit.factory.AnalysisEngineFactory
 import org.uimafit.pipeline.SimplePipeline
+import org.uimafit.util.JCasUtil.select
 import org.apache.uima.util.CasCreationUtils
-import org.uimafit.factory.ExternalResourceFactory.{createExternalResourceDescription, createDependencyAndBind}
-import ru.ksu.niimm.cll.uima.morph.opencorpora.resource.{CachedSerializedDictionaryResource, MorphDictionaryHolder}
+import org.uimafit.factory.ExternalResourceFactory.createExternalResourceDescription
+import ru.ksu.niimm.cll.uima.morph.opencorpora.resource.CachedSerializedDictionaryResource
 import java.io.File
 import ru.ksu.niimm.cll.uima.morph.opencorpora.MorphologyAnnotator
-import java.util.Arrays
+import org.opencorpora.cas.{Wordform, Word}
+import org.apache.uima.cas.FeatureStructure
 
 /**
  * Created by fsqcds on 07/05/14.
  */
 class LemmatizerSpec extends FlatSpec with Matchers {
-  "Lemmatizer" should "be runnable" in {
+  "Lemmatizer" should "generate correct lemmas" in {
     val posPipelineDesc = GeneratePipelineDescriptorForTPSAnnotator.getDescription
     val extDictDesc = createExternalResourceDescription(classOf[CachedSerializedDictionaryResource], new File("dict.opcorpora.ser"))
     val lemmatizerDesc = AnalysisEngineFactory.createPrimitiveDescription(classOf[Lemmatizer], MorphologyAnnotator.RESOURCE_KEY_DICTIONARY, extDictDesc)
     val aggregateDesc = AnalysisEngineFactory.createAggregateDescription(posPipelineDesc, lemmatizerDesc)
     val jCas = CasCreationUtils.createCas(aggregateDesc).getJCas
-    jCas.setDocumentText("Какой-то русский текст написан здесь.")
+    jCas.setDocumentText("Душа моя озарена неземной радостью")
     SimplePipeline.runPipeline(jCas, aggregateDesc)
+    val lemmas = select(jCas, classOf[Word]).flatMap((word: Word) => {
+      word.getWordforms.toArray.map((wordformFS: FeatureStructure) => {
+        wordformFS.asInstanceOf[Wordform].getLemma
+      })
+    })
+    lemmas should be(List("душа", "мой", "озарён", "неземной", "радость"))
   }
 }
