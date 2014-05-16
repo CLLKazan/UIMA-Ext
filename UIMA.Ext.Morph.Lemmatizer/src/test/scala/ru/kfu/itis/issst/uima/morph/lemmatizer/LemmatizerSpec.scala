0c44598e968eda6c6fabcdc38a61e0b036e4c008
fsqcds <fsqcds@gmail.com>
2014-05-16 18:10:55 +0400

    Lemmatizer test stub

--- UIMA.Ext.Morph.Lemmatizer/src/test/scala/ru/kfu/itis/issst/uima/morph/lemmatizer/LemmatizerSpec.scala
+++ UIMA.Ext.Morph.Lemmatizer/src/test/scala/ru/kfu/itis/issst/uima/morph/lemmatizer/LemmatizerSpec.scala
@@ -1,12 +1,21 @@
 package ru.kfu.itis.issst.uima.morph.lemmatizer
 
 import org.scalatest._
+import ru.ksu.niimm.cll.uima.morph.ml.GeneratePipelineDescriptorForTPSAnnotator
+import org.uimafit.factory.AnalysisEngineFactory
+import org.uimafit.pipeline.SimplePipeline
+import org.apache.uima.util.CasCreationUtils
 
 /**
  * Created by fsqcds on 07/05/14.
  */
 class LemmatizerSpec extends FlatSpec with Matchers {
-  "true" should "be true" in {
-    true should be (true)
+  "Lemmatizer" should "do something" in {
+    val posPipelineDesc = GeneratePipelineDescriptorForTPSAnnotator.getDescription;
+    val lemmatizerDesc = AnalysisEngineFactory.createPrimitiveDescription(classOf[Lemmatizer])
+    val aggregateDesc = AnalysisEngineFactory.createAggregateDescription(posPipelineDesc, lemmatizerDesc)
+    val jCas = CasCreationUtils.createCas(aggregateDesc).getJCas;
+    jCas.setDocumentText("Какой-то русский текст написан здесь.")
+    SimplePipeline.runPipeline(jCas, posPipelineDesc, lemmatizerDesc)
   }
 }
