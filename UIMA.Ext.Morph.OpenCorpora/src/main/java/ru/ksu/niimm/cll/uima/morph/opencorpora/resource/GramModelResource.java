d857578dba98a159b70aa191e176b90a2ce0695b
Rinat Gareev <gareev-rm@yandex.ru>
2014-06-11 18:24:17 +0400

    +GramModelResource

---
+++ UIMA.Ext.Morph.OpenCorpora/src/main/java/ru/ksu/niimm/cll/uima/morph/opencorpora/resource/GramModelResource.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package ru.ksu.niimm.cll.uima.morph.opencorpora.resource;
+
+import org.apache.uima.resource.DataResource;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.resource.SharedResourceObject;
+
+/**
+ * @author Rinat Gareev (Kazan Federal University)
+ * 
+ */
+public class GramModelResource implements GramModelHolder, SharedResourceObject {
+
+	private GramModel gramModel;
+
+	@Override
+	public void load(DataResource dr) throws ResourceInitializationException {
+		try {
+			gramModel = GramModelDeserializer
+					.from(dr.getInputStream(), String.valueOf(dr.getUrl()));
+		} catch (Exception e) {
+			throw new ResourceInitializationException(e);
+		}
+	}
+
+	@Override
+	public GramModel getGramModel() {
+		return gramModel;
+	}
+
+}
