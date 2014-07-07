43850c31593b025ca20dba8d89e4de75ddc83f60
Rinat Gareev <gareev-rm@yandex.ru>
2014-07-07 15:38:52 +0400

    +LemmaFeatureExtractor

---
+++ UIMA.Ext.Morph.OpenCorpora/src/main/java/ru/ksu/niimm/cll/uima/morph/ml/LemmaFeatureExtractor.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package ru.ksu.niimm.cll.uima.morph.ml;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+import org.cleartk.classifier.Feature;
+import org.cleartk.classifier.feature.extractor.CleartkExtractorException;
+import org.cleartk.classifier.feature.extractor.simple.SimpleNamedFeatureExtractor;
+import org.opencorpora.cas.Word;
+import org.opencorpora.cas.Wordform;
+import org.uimafit.util.FSCollectionFactory;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
+/**
+ * @author Rinat Gareev (Kazan Federal University)
+ * 
+ */
+public class LemmaFeatureExtractor implements SimpleNamedFeatureExtractor {
+
+	public static final String FEATURE_NAME = "Lemma";
+
+	@Override
+	public List<Feature> extract(JCas view, Annotation focusAnnotation)
+			throws CleartkExtractorException {
+		Word focusWord = PUtils.getWordAnno(view, focusAnnotation);
+		if (focusWord == null || focusWord.getWordforms() == null) {
+			return ImmutableList.of();
+		}
+		Collection<Wordform> wfs = FSCollectionFactory.create(focusWord.getWordforms(),
+				Wordform.class);
+		Set<String> lemmas = Sets.newHashSet();
+		for (Wordform wf : wfs) {
+			if (wf.getLemma() != null) {
+				lemmas.add(wf.getLemma());
+			}
+		}
+		return Lists.newArrayList(Collections2.transform(lemmas, lemma2Feature));
+	}
+
+	@Override
+	public String getFeatureName() {
+		return FEATURE_NAME;
+	}
+
+	private static final Function<String, Feature> lemma2Feature = new Function<String, Feature>() {
+		@Override
+		public Feature apply(String lemma) {
+			return new Feature(FEATURE_NAME, lemma);
+		}
+	};
+}
