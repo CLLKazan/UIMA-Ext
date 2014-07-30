199b7182e857be7039896e9b0407301e17ba5320
Rinat Gareev <gareev-rm@yandex.ru>
2014-07-30 20:33:00 +0400

    [TXCAT-7] + javadocs in TokenizerAPI

--- UIMA.Ext.Tokenizer.API/src/main/java/ru/kfu/itis/issst/uima/tokenizer/TokenizerAPI.java
+++ UIMA.Ext.Tokenizer.API/src/main/java/ru/kfu/itis/issst/uima/tokenizer/TokenizerAPI.java
@@ -13,14 +13,25 @@ import org.apache.uima.resource.metadata.impl.Import_impl;
 import org.uimafit.factory.AnalysisEngineFactory;
 import org.uimafit.factory.TypeSystemDescriptionFactory;
 
+import ru.kfu.itis.cll.uima.util.PipelineDescriptorUtils;
+
 /**
  * @author Rinat Gareev (Kazan Federal University)
  * 
  */
 public class TokenizerAPI {
 
+	/**
+	 * A name of analysis engine description that can be imported. A tokenizer
+	 * implementation should provide its description at this location either in
+	 * classpath or UIMA datapath.
+	 */
 	public static final String AE_TOKENIZER = "ru.kfu.itis.issst.uima.tokenizer.tokenizer-ae";
 
+	/**
+	 * A name of type-system description that define types produced by tokenizer
+	 * implementations.
+	 */
 	public static final String TYPESYSTEM_TOKENIZER = "ru.kfu.cll.uima.tokenizer.tokenizer-TypeSystem";
 
 	/**
@@ -34,14 +45,29 @@ public class TokenizerAPI {
 	 */
 	public static final String DEFAULT_SPAN_TYPE = "uima.tcas.DocumentAnnotation";
 
+	/**
+	 * @return type-system description instance
+	 */
 	public static TypeSystemDescription getTypeSystemDescription() {
 		return TypeSystemDescriptionFactory.createTypeSystemDescription(TYPESYSTEM_TOKENIZER);
 	}
 
+	/**
+	 * @return AE description instance
+	 * @throws UIMAException
+	 * @throws IOException
+	 */
 	public static AnalysisEngineDescription getAEDescription() throws UIMAException, IOException {
 		return AnalysisEngineFactory.createAnalysisEngineDescription(AE_TOKENIZER);
 	}
 
+	/**
+	 * @return import instance. This is preferred way to include the AE into
+	 *         pipeline, especially when a pipeline descriptor is expected to be
+	 *         serialized into XML.
+	 * @see PipelineDescriptorUtils#createAggregateDescription(java.util.List,
+	 *      java.util.List)
+	 */
 	public static Import getAEImport() {
 		Import result = new Import_impl();
 		result.setName(AE_TOKENIZER);
