7738346cedcf1dad0ec707d12d4d4eb6ed1172d1
Rinat Gareev <gareev-rm@yandex.ru>
2014-08-14 15:44:32 +0400

    [TXCAT-59][Commons] +ErrorProducer and its description

---
+++ UIMA.Ext.Commons/src/main/java/ru/kfu/itis/cll/uima/annotator/ErrorProducer.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package ru.kfu.itis.cll.uima.annotator;
+
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.CAS;
+import org.uimafit.component.CasAnnotator_ImplBase;
+
+/**
+ * An annotator that checks whether a document text contains '%ERROR%' substring
+ * and if it does then throws {@link AnalysisEngineProcessException}.
+ * <p>
+ * To be used in tests.
+ * 
+ * @author Rinat Gareev (Kazan Federal University)
+ * 
+ */
+public class ErrorProducer extends CasAnnotator_ImplBase {
+
+	public static final String ERROR_SUBSTRING = "%ERROR%";
+
+	@Override
+	public void process(CAS cas) throws AnalysisEngineProcessException {
+		if (cas.getDocumentText().contains(ERROR_SUBSTRING)) {
+			throw new AnalysisEngineProcessException(
+					new Exception("ErrorProducer is on duty!"));
+		}
+	}
+}
---
+++ UIMA.Ext.Commons/src/test/java/ru/kfu/itis/cll/uima/annotator/GenerateErrorProducerDesc.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package ru.kfu.itis.cll.uima.annotator;
+
+import static org.uimafit.factory.AnalysisEngineFactory.createPrimitiveDescription;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.xml.sax.SAXException;
+
+/**
+ * @author Rinat Gareev (Kazan Federal University)
+ * 
+ */
+public class GenerateErrorProducerDesc {
+
+	public static void main(String[] args) throws ResourceInitializationException, SAXException,
+			IOException {
+		AnalysisEngineDescription desc = createPrimitiveDescription(ErrorProducer.class);
+		FileOutputStream out = FileUtils.openOutputStream(new File(
+				"src/main/resources/ru/kfu/itis/cll/uima/annotator/ErrorProducer.xml"));
+		try {
+			desc.toXML(out);
+		} finally {
+			IOUtils.closeQuietly(out);
+		}
+	}
+
+}
