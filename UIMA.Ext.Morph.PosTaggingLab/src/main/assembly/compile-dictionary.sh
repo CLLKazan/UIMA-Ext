0551c7c3a2b74ee454b2ac69d08e8c72809d7da4
Rinat Gareev <gareev-rm@yandex.ru>
2014-09-26 14:24:33 +0400

    fixed the dictionary compilation script

--- UIMA.Ext.Morph.PosTaggingLab/src/main/assembly/compile-dictionary.sh
+++ UIMA.Ext.Morph.PosTaggingLab/src/main/assembly/compile-dictionary.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 . setup.sh
-java -Xmx1400m -cp lib/${project.build.finalName}.jar ru.ksu.niimm.cll.uima.morph.opencorpora.resource.XmlDictionaryParser "${opencorpora_home}/dict.opcorpora.xml" "${opencorpora_home}/dict.opcorpora.ser"
+java -Xmx1500m -cp lib/${project.build.finalName}.jar ru.ksu.niimm.cll.uima.morph.opencorpora.resource.XmlDictionaryParserLauncher \
+	-i "${opencorpora_home}/dict.opcorpora.xml" \
+	-o "${opencorpora_home}/dict.opcorpora.ser" \
+	--dict-extension-class ru.ksu.niimm.cll.uima.morph.ruscorpora.RNCDictionaryExtension 
