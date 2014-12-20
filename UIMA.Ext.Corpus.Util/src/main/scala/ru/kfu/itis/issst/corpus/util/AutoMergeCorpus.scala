a1958f56ac149902c41f384d6c2228cdace9a6dd
Rinat Gareev <gareev-rm@yandex.ru>
2014-12-20 17:28:56 +0300

    Finished AutoMergeCorpus impl

--- UIMA.Ext.Corpus.Util/src/main/scala/ru/kfu/itis/issst/corpus/util/AutoMergeCorpus.scala
+++ UIMA.Ext.Corpus.Util/src/main/scala/ru/kfu/itis/issst/corpus/util/AutoMergeCorpus.scala
@@ -3,24 +3,27 @@ package ru.kfu.itis.issst.corpus.util
 import scopt.OptionParser
 import AutoMergeCorpus._
 import java.io.File
-import ru.kfu.itis.issst.corpus.statistics.dao.corpus.{ CorpusDAO => ROCorpusDAO }
+import ru.kfu.itis.issst.corpus.statistics.dao.corpus.CorpusDAO
 import ru.kfu.itis.issst.corpus.statistics.dao.corpus.XmiFileTreeCorpusDAO
 import java.net.URI
 import scala.collection.JavaConversions._
 import org.apache.uima.resource.metadata.TypeSystemDescription
 import org.apache.uima.util.CasCreationUtils
 import org.apache.uima.cas.CAS
-import ru.kfu.itis.issst.corpus.dao.CorpusDAO
 import org.apache.uima.cas.Type
 import ru.kfu.itis.cll.uima.util.AnnotatorUtils
 import org.apache.uima.cas.TypeSystem
 import org.apache.uima.cas.FSIterator
 import org.apache.uima.cas.text.AnnotationFS
+import scala.collection.mutable.ListBuffer
+import com.typesafe.scalalogging.StrictLogging
+import scala.collection.{ mutable => mu }
+import ru.kfu.itis.cll.uima.cas.AnnotationUtils
 
-private class AutoMergeCorpus(srcCorpus: ROCorpusDAO, outputCorpus: CorpusDAO,
-                              typeSystemDesc: TypeSystemDescription,
-                              mergerId: String,
-                              mergeCfg: MergeConfig) {
+private class AutoMergeCorpus(srcCorpus: CorpusDAO, outputCorpus: CorpusDAO,
+  typeSystemDesc: TypeSystemDescription,
+  mergerId: String,
+  mergeCfg: MergeConfig) extends StrictLogging {
 
   private val outCas = CasCreationUtils.createCas(typeSystemDesc, null, null)
   private type AnnoIterator = FSIterator[AnnotationFS]
@@ -42,10 +45,12 @@ private class AutoMergeCorpus(srcCorpus: ROCorpusDAO, outputCorpus: CorpusDAO,
     if (outputCorpus.hasDocument(docUri, mergerId)) {
       outCas.reset()
       outputCorpus.getDocumentCas(docUri, mergerId, outCas)
+      logger.info(s"About to merge $docUri from $annotatorId")
       merge(cas)
       outputCorpus.persist(docUri, mergerId, outCas)
     } else {
       outputCorpus.persist(docUri, mergerId, cas)
+      logger.info(s"$docUri is initialized using data from $annotatorId")
     }
   }
 
@@ -54,42 +59,58 @@ private class AutoMergeCorpus(srcCorpus: ROCorpusDAO, outputCorpus: CorpusDAO,
       val annoType = getTypeSystem.getType(annoTypeName)
       AnnotatorUtils.annotationTypeExist(annoTypeName, annoType)
       merge(otherCas, annoType)
+      logger.debug(s"Finished merging for type $annoTypeName")
     }
   }
 
   private def merge(otherCas: CAS, annoType: Type) {
-    val outIter = outCas.getAnnotationIndex(annoType).iterator()
-    outIter.moveToFirst()
-    val otherIter = otherCas.getAnnotationIndex(annoType).iterator()
-    otherIter.moveToFirst()
-    merge(outIter, otherIter)
+    val outIdx = outCas.getAnnotationIndex(annoType)
+    // we should memorize outIdx annotations now because this idx can change here
+    val outAll = List.empty ++ outIdx
+    val outTouched = mu.Set.empty[AnnotationFS]
+    //
+    val overlapIdx = AnnotationUtils.createOverlapIndex(outIdx.iterator());
+
+    val inputIdx = otherCas.getAnnotationIndex(annoType).iterator()
+
+    for (candAnno <- inputIdx) {
+      val overlaps = List.empty ++ overlapIdx.getOverlapping(candAnno.getBegin, candAnno.getEnd)
+      overlaps match {
+        case List() => {
+          add(candAnno)
+          logger.info("{} is unconfirmed. Added.", formatAnno(candAnno))
+        }
+        case List(single) if sameBoundaries(candAnno, single) => {
+          // do nothing
+          logger.debug("{} is confirmed", formatAnno(candAnno))
+        }
+        case _ => {
+          // search for exact boundaries
+          if (overlaps.exists(sameBoundaries(candAnno, _))) {
+            // do nothing
+            logger.debug("{} is confirmed", formatAnno(candAnno))
+          } else {
+            // conflict
+            add(candAnno)
+            logger.warn("Conflict: {}\nIncoming: {}",
+              overlaps.map(formatAnno), formatAnno(candAnno))
+          }
+        }
+      }
+      outTouched ++= overlaps
+    }
+    // report about untouched out annotations as unconfirmed
+    for (
+      uncAnno <- outAll if !outTouched.contains(uncAnno)
+    ) {
+      logger.info("{} is unconfirmed", formatAnno(uncAnno))
+    }
   }
 
-  private def merge(outIter: AnnoIterator, inIter: AnnoIterator): Unit =
-    if (inIter.isValid()) {
-      val candAnno = inIter.get()
-      //
-      outIter.moveTo(candAnno)
-      if (!outIter.isValid()) outIter.moveToLast()
-      var overlaps: List[AnnotationFS] = Nil
-      while (outIter.isValid() && outIter.get.getEnd > candAnno.getBegin) {
-        if (outIter.get.getBegin < candAnno.getEnd) // i.e. overlaps
-          overlaps ::= outIter.get
-        outIter.moveToPrevious()
-      }
-      overlaps match {
-        case List() => add(candAnno) // TODO REPORT
-        case List(single) if sameBoundaries(candAnno, single) => // do nothing
-        case _ => add(candAnno) // TODO REPORT about this conflict
-      }
-      // TODO
-      ???
-      //
-      inIter.moveToNext()
-      merge(outIter, inIter)
-    }
-  
-  private def sameBoundaries(first:AnnotationFS, second:AnnotationFS):Boolean =
+  private def formatAnno(anno: AnnotationFS) =
+    s"${anno.getType.getShortName}[${anno.getBegin},${anno.getCoveredText}]"
+
+  private def sameBoundaries(first: AnnotationFS, second: AnnotationFS): Boolean =
     first.getBegin == second.getBegin && first.getEnd == second.getEnd
 
   private def add(srcAnno: AnnotationFS) {
@@ -115,12 +136,12 @@ object AutoMergeCorpus {
         (arg, cfg) => cfg.copy(outputCorpusBaseDir = arg))
       opt[String]('m', "merger-id") optional () valueName ("<annotator-id>") action (
         (arg, cfg) => cfg.copy(mergerId = arg))
-      opt[File]('c', "merge-config") required () valueName ("<dir>") action (
+      opt[File]('c', "merge-config") required () valueName ("<file>") action (
         (arg, cfg) => cfg.copy(mergeCfgFile = arg))
     }
     cmdParser.parse(args, Config()) match {
       case Some(cfg) =>
-        val srcCorpus = toROCorpus(cfg.srcCorpusBaseDir)
+        val srcCorpus = toCorpus(cfg.srcCorpusBaseDir)
         val outCorpus = toCorpus(cfg.outputCorpusBaseDir)
         val tsDesc = getTypeSystemDesc(cfg.srcCorpusBaseDir)
         val mergeCfg = MergeConfig.read(cfg.mergeCfgFile)
@@ -129,9 +150,7 @@ object AutoMergeCorpus {
     }
   }
 
-  private def toROCorpus(baseDir: File): ROCorpusDAO = new XmiFileTreeCorpusDAO(baseDir.getPath)
-
-  private def toCorpus(baseDir: File): CorpusDAO = ??? // TODO
+  private def toCorpus(baseDir: File): CorpusDAO = new XmiFileTreeCorpusDAO(baseDir.getPath)
 
   private def getTypeSystemDesc(corpusDir: File) = XmiFileTreeCorpusDAO.getTypeSystem(corpusDir.getPath)
 }
