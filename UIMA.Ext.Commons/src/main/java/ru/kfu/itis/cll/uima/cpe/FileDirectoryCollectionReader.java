244ad44a3ffb37aa229269ed72c679e938f1af9e
Rinat Gareev <gareev-rm@yandex.ru>
2015-02-03 18:07:33 +0300

    FileDirectoryCollectionReaders read files in subdirectories and set relative URI of a source by default

--- UIMA.Ext.Commons/src/main/java/ru/kfu/itis/cll/uima/cpe/FileDirectoryCollectionReader.java
+++ UIMA.Ext.Commons/src/main/java/ru/kfu/itis/cll/uima/cpe/FileDirectoryCollectionReader.java
@@ -3,11 +3,12 @@
  */
 package ru.kfu.itis.cll.uima.cpe;
 
+import static org.uimafit.factory.TypeSystemDescriptionFactory.createTypeSystemDescription;
+
 import java.io.File;
-import java.io.FileFilter;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 import org.apache.commons.io.FileUtils;
@@ -17,10 +18,14 @@ import org.apache.uima.UimaContext;
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
 import org.apache.uima.collection.CollectionException;
+import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
 import org.apache.uima.util.Progress;
+import org.apache.uima.util.ProgressImpl;
 import org.uimafit.component.CasCollectionReader_ImplBase;
 import org.uimafit.descriptor.ConfigurationParameter;
+import org.uimafit.factory.CollectionReaderFactory;
 
 import ru.kfu.itis.cll.uima.commons.DocumentMetadata;
 
@@ -32,9 +37,20 @@ import com.google.common.collect.Lists;
  */
 public class FileDirectoryCollectionReader extends CasCollectionReader_ImplBase {
 
-	public static final String PARAM_DIRECTORY_PATH = "DirectoryPath";
-	public static final String PARAM_FILE_EXTENSION = "FileExtension";
-	public static final String PARAM_ENCODING = "Encoding";
+	public static CollectionReaderDescription createDescription(File inputDir)
+			throws ResourceInitializationException {
+		TypeSystemDescription inputTSD = createTypeSystemDescription(
+				"ru.kfu.itis.cll.uima.commons.Commons-TypeSystem");
+		return CollectionReaderFactory.createDescription(
+				FileDirectoryCollectionReader.class,
+				inputTSD,
+				PARAM_DIRECTORY_PATH, inputDir);
+	}
+
+	public static final String PARAM_DIRECTORY_PATH = "directoryPath";
+	public static final String PARAM_FILE_EXTENSION = "fileExtension";
+	public static final String PARAM_ENCODING = "encoding";
+	public static final String PARAM_SET_RELATIVE_URI = "setRelativeURI";
 
 	// config
 	@ConfigurationParameter(name = PARAM_DIRECTORY_PATH, mandatory = true)
@@ -43,10 +59,12 @@ public class FileDirectoryCollectionReader extends CasCollectionReader_ImplBase 
 	private String fileExtension;
 	@ConfigurationParameter(name = PARAM_ENCODING, defaultValue = "utf-8")
 	private String encoding;
+	@ConfigurationParameter(name = PARAM_SET_RELATIVE_URI, defaultValue = "true")
+	private boolean setRelativeURI;
 	// derived
-	private List<File> files;
+	private ArrayList<File> files;
 	// state
-	private Iterator<File> fileIter;
+	private int lastReadFileIdx;
 
 	@Override
 	public void initialize(UimaContext ctx) throws ResourceInitializationException {
@@ -56,9 +74,10 @@ public class FileDirectoryCollectionReader extends CasCollectionReader_ImplBase 
 					"%s is not existing file directory", directory));
 		}
 		IOFileFilter fileFilter = FileFilterUtils.suffixFileFilter(fileExtension);
-		files = Lists.newArrayList(directory.listFiles((FileFilter) fileFilter));
-
-		fileIter = files.iterator();
+		IOFileFilter subdirFilter = FileFilterUtils.trueFileFilter();
+		files = Lists.newArrayList(FileUtils.listFiles(directory, fileFilter, subdirFilter));
+		//
+		lastReadFileIdx = -1;
 	}
 
 	/**
@@ -69,15 +88,28 @@ public class FileDirectoryCollectionReader extends CasCollectionReader_ImplBase 
 		if (!hasNext()) {
 			throw new CollectionException(new NoSuchElementException());
 		}
-		File file = fileIter.next();
+		final int curFileIdx = lastReadFileIdx + 1;
+		File file = files.get(curFileIdx);
+		lastReadFileIdx = curFileIdx;
+		//
 		String fileContent = FileUtils.readFileToString(file, encoding);
 		aCAS.setDocumentText(fileContent);
 		try {
 			DocumentMetadata docMeta = new DocumentMetadata(aCAS.getJCas());
-			docMeta.setSourceUri(file.toURI().toString());
+			docMeta.setSourceUri(getURIForMetadata(file).toString());
 			docMeta.addToIndexes();
 		} catch (CASException e) {
 			throw new IllegalStateException(e);
+		}
+	}
+
+	private URI getURIForMetadata(File f) {
+		URI fURI = f.toURI();
+		if (setRelativeURI) {
+			URI dirURI = directory.toURI();
+			return dirURI.relativize(fURI);
+		} else {
+			return fURI;
 		}
 	}
 
@@ -86,7 +118,7 @@ public class FileDirectoryCollectionReader extends CasCollectionReader_ImplBase 
 	 */
 	@Override
 	public boolean hasNext() throws IOException, CollectionException {
-		return fileIter.hasNext();
+		return (lastReadFileIdx + 1) < files.size();
 	}
 
 	/**
@@ -94,14 +126,9 @@ public class FileDirectoryCollectionReader extends CasCollectionReader_ImplBase 
 	 */
 	@Override
 	public Progress[] getProgress() {
-		return null;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void close() throws IOException {
+		return new Progress[] {
+				new ProgressImpl(lastReadFileIdx + 1, files.size(), Progress.ENTITIES)
+		};
 	}
 
 }
