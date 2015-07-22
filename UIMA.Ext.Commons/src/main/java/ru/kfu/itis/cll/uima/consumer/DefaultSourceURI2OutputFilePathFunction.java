bd6c9543110b9df84642c08762bc036a7674851e
Rinat Gareev <gareev-rm@yandex.ru>
2015-07-22 20:06:25 +0300

    Default URI -> output path function adds XMI extension to a filename

--- UIMA.Ext.Commons/src/main/java/ru/kfu/itis/cll/uima/consumer/DefaultSourceURI2OutputFilePathFunction.java
+++ UIMA.Ext.Commons/src/main/java/ru/kfu/itis/cll/uima/consumer/DefaultSourceURI2OutputFilePathFunction.java
@@ -1,6 +1,7 @@
 package ru.kfu.itis.cll.uima.consumer;
 
 import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
 import ru.kfu.itis.cll.uima.commons.DocumentMetadata;
 
 import java.net.URI;
@@ -10,11 +11,15 @@ import java.nio.file.Paths;
 import static java.lang.String.format;
 
 /**
- * Simply extracts 'path' part from a given source URI . It means that opaque URIs are not supported.
+ * Simply extracts 'path' part from a given source URI and adds ".xmi" suffix.
+ * It means that opaque URIs are not supported.
  *
  * @author Rinat Gareev
  */
 public class DefaultSourceURI2OutputFilePathFunction implements Function<DocumentMetadata, Path> {
+
+    public static final String XMI_FILE_EXTENSION = ".xmi";
+
     @Override
     public Path apply(DocumentMetadata metaAnno) {
         String uriStr = metaAnno.getSourceUri();
@@ -25,6 +30,11 @@ public class DefaultSourceURI2OutputFilePathFunction implements Function<Documen
         if (uri.isOpaque()) {
             throw new IllegalArgumentException(format("Opaque URIs are not supported: %s", uri));
         }
-        return Paths.get(uri.getPath());
+        String path = uri.getPath();
+        Preconditions.checkState(path != null, "URI path is null: " + uri);
+        if (!path.endsWith(XMI_FILE_EXTENSION)) {
+            path += XMI_FILE_EXTENSION;
+        }
+        return Paths.get(path);
     }
 }
